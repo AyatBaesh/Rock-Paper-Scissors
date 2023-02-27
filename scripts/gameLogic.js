f29a43506efdf39637519dc8ef69c3aee94ca38b
buyshow <ayatbaeshev@gmail.com>
2023-02-27 15:05:47 +0100

    added roundCounter and playAgain button, work in progress

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,86 +1,92 @@
-// function getComputerChoice(){//making random computer choice
-//     const rockPaperScissors = ["rock", "paper", "scissors"];
-//     let randomElement = Math.floor(Math.random() * rockPaperScissors.length);
 
-//     return rockPaperScissors[randomElement];
-// }
 
-// function round(playerChoice, computerChoice){
-//     let roundWinner = "";
-//     //function to play 1 round and return the result
-//     switch(playerChoice + computerChoice){
-//         default:
-//             roundWinner = "Tie game!";
-//             break;
-//         case "scissorspaper":
-//         case "rockscissors":
-//         case "paperrock":
-//             roundWinner = "player";
-//             break;
 
-//         case "scissorsrock":
-//         case "rockpaper":
-//         case "paperscissors":
-//             roundWinner = "computer";
-//             break;
-//     }
-//     return roundWinner;
+function getComputerChoice(){//making random computer choice
+    const rockPaperScissors = ["rock", "paper", "scissors"];
+    let randomElement = Math.floor(Math.random() * rockPaperScissors.length);
 
-// }
+    return rockPaperScissors[randomElement];
+}
 
-// function getPlayerChoice(playerChoiceButtons){
-//     let playerChoice = "";
-//     playerChoiceButtons.forEach(chosenButton  => {
-//         chosenButton.disabled = false;
-//         chosenButton.addEventListener("click", () =>{
-//             playerChoice = chosenButton.value;
-//         });
+function round(playerChoice, computerChoice){
+    let roundWinner = "";
+    //function to play 1 round and return the result
+    switch(playerChoice + computerChoice){
+        default:
+            roundWinner = "Tie game!";
+            break;
+        case "scissorspaper":
+        case "rockscissors":
+        case "paperrock":
+            roundWinner = "player";
+            break;
+
+        case "scissorsrock":
+        case "rockpaper":
+        case "paperscissors":
+            roundWinner = "computer";
+            break;
+    }
+    return roundWinner;
+
+}
+function updateScore(scoreCounter, score){
+    scoreCounter.textContent =  score;
+}
+
+function playGame(playerChoiceButtons, computerScore, playerScore, roundCount){
+    let playerWinsCount = 0;
+    let computerWinsCount = 0;
+    let roundCounter = 0;
+    let roundResult = "";
+    playerChoiceButtons.forEach(chosenButton => {
+        chosenButton.addEventListener("click", () =>{//waits for user choice and calls round()
+            roundResult = round(chosenButton.value, getComputerChoice());
+            switch(roundResult){//checks for roundResult and updates scores
+                default:
+                        console.log("Something wrong, I can feel it...");
+                        break;
+                
+                case "Tie game!":
+                    console.log("Round draw!");
+                    break;
+                
+                case "player":
+                    playerWinsCount++;
+                    updateScore(playerScore, playerWinsCount);
+                    console.log("PLAYER WON THIS ROUND!");
+                    console.log(`playerScore = ${playerWinsCount}`);
+                    break;
+                
+                case "computer":
+                    computerWinsCount++;
+                    updateScore(computerScore, computerWinsCount);
+                    console.log("COMPUTER WON THIS ROUND!");
+                    console.log(`computerScore = ${computerWinsCount}`);
+                    break;
+            }
+            roundCounter++;
+            roundCount.textContent = roundCounter;
+            if(computerWinsCount === 5){
+                console.log("You lost this game :(");
+                computerWinsCount = 0;
+                playerWinsCount = 0;
+                playerScore.textContent = playerWinsCount;
+                computerScore.textContent = computerWinsCount;
+                return;
+
+            }else if(playerWinsCount === 5){
+                console.log("You won this game!");
+                computerWinsCount = 0;
+                playerWinsCount = 0;
+                playerScore.textContent = playerWinsCount;
+                computerScore.textContent = computerWinsCount;
+                return;
+        }
+            
+        });
         
-//     });
-//     return playerChoice;
-// }
-// function game(playerChoiceButtons, playerScore, computerScore){
-//     let playerWinsCount = 0;
-//     let computerWinsCount = 0;
-//     let playerChoice = getPlayerChoice(playerChoiceButtons);
-//     let roundResult = round(playerChoice, getComputerChoice);
+    });
 
-//         switch(roundResult){
-//             default:
-//                     console.log("Something wrong, I can feel it...");
-//                     break;
-        
-//             case "Tie game!":
-//                 console.log("Round draw!");
-//                 break;
-        
-//             case "player":
-//                 playerWinsCount++;
-//                 playerScore.textContent = playerWinsCount;
-//                 console.log("PLAYER WON THIS ROUND!");
-//                 console.log(`playerScore = ${playerWinsCount}`);
-//                 break;
-        
-//             case "computer":
-//                 computerWinsCount++;
-//                 computerScore.textContent = computerWinsCount;
-//                 console.log("COMPUTER WON THIS ROUND!");
-//                 console.log(`computerScore = ${computerWinsCount}`);
-//                 break;
-//         }
-    
-        
-
-//     // if(computerWinsCount === 5){
-//     //         console.log("You lost this game :(");
-
-//     //         return;
-//     //     }else if(playerWinsCount === 5){
-//     //         console.log("You won this game!");
-
-//     //         return;
-//     //     }
-    
-
-//     }
-// export{getComputerChoice, getPlayerChoice, round, game}
+}
+export{getComputerChoice,round, playGame}
--- scripts/index.js
+++ scripts/index.js
@@ -1,100 +1,17 @@
-// import {getComputerChoice, round, game} from "./gameLogic.js";
+import {getComputerChoice, round, playGame} from "./gameLogic.js";
 const playerChoiceButtons = document.querySelectorAll(".player-choice");
 let playerScore = document.querySelector("#playerScore");
 let computerScore = document.querySelector("#computerScore");
+let roundCount = document.querySelector("#roundCounter");
 const startButton = document.querySelector("#gameStart");
 
 
-function getComputerChoice(){//making random computer choice
-    const rockPaperScissors = ["rock", "paper", "scissors"];
-    let randomElement = Math.floor(Math.random() * rockPaperScissors.length);
-
-    return rockPaperScissors[randomElement];
-}
-
-function round(playerChoice, computerChoice){
-    let roundWinner = "";
-    //function to play 1 round and return the result
-    switch(playerChoice + computerChoice){
-        default:
-            roundWinner = "Tie game!";
-            break;
-        case "scissorspaper":
-        case "rockscissors":
-        case "paperrock":
-            roundWinner = "player";
-            break;
-
-        case "scissorsrock":
-        case "rockpaper":
-        case "paperscissors":
-            roundWinner = "computer";
-            break;
-    }
-    return roundWinner;
-
-}
-
-
-function playGame(playerChoiceButtons, computerScore, playerScore){
-    let playerWinsCount = 0;
-    let computerWinsCount = 0;
-    let roundResult = "";
-    playerChoiceButtons.forEach(chosenButton => {
-        chosenButton.addEventListener("click", () =>{
-            roundResult = round(chosenButton.value, getComputerChoice());
-            switch(roundResult){
-                default:
-                        console.log("Something wrong, I can feel it...");
-                        break;
-                
-                case "Tie game!":
-                    console.log("Round draw!");
-                    break;
-                
-                case "player":
-                    playerWinsCount++;
-                    playerScore.textContent = playerWinsCount;
-                    console.log("PLAYER WON THIS ROUND!");
-                    console.log(`playerScore = ${playerWinsCount}`);
-                    break;
-                
-                case "computer":
-                    computerWinsCount++;
-                    computerScore.textContent = computerWinsCount;
-                    console.log("COMPUTER WON THIS ROUND!");
-                    console.log(`computerScore = ${computerWinsCount}`);
-                    break;
-            }
-
-    if(computerWinsCount === 5){
-            console.log("You lost this game :(");
-            computerWinsCount = 0;
-            playerWinsCount = 0;
-            playerScore.textContent = playerWinsCount;
-            computerScore.textContent = computerWinsCount;
-            return;
-        }else if(playerWinsCount === 5){
-            console.log("You won this game!");
-            computerWinsCount = 0;
-            playerWinsCount = 0;
-            playerScore.textContent = playerWinsCount;
-            computerScore.textContent = computerWinsCount;
-
-            return;
-        }
-            
-        });
-        
-    });
-
-}
 startButton.addEventListener("click", ()=>{
     playerChoiceButtons.forEach(chosenButton => {
         chosenButton.disabled = false;
         
     });
-    playGame(playerChoiceButtons, computerScore, playerScore);
+    playGame(playerChoiceButtons, computerScore, playerScore, roundCount);
 });
 
 
