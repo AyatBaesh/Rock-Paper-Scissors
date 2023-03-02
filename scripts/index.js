392d8db8b9be0f3ae4008e03acc5cb5178751a4e
buyshow <ayatbaeshev@gmail.com>
2023-03-02 20:27:02 +0100

    finished game functionality and refactored index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,117 +1,117 @@
-import {getComputerChoice, round, playGame} from "./gameLogic.js";
-const playerChoiceButtons = document.querySelectorAll(".player-choice");
-let playerScore = document.querySelector("#playerScore");
-let computerScore = document.querySelector("#computerScore");
-let roundCount = document.querySelector("#roundCounter");
-const startButton = document.querySelector("#gameStart");
+// DOM elements
+const computerScoreEl = document.querySelector("#computerScore");
+const playerScoreEl = document.querySelector("#playerScore");
+const roundsPlayedEl = document.querySelector("#roundCounter");
+const choiceButtons = document.querySelectorAll(".player-choice");
+const gameResultsSection = document.querySelector(".game-results");
+const newGameButton = document.querySelector("#newGame");
 
+// Game state variables
+let computerScore = 0;
+let playerScore = 0;
+let roundCounter = 0;
 
-startButton.addEventListener("click", ()=>{
-    playerChoiceButtons.forEach(chosenButton => {
-        chosenButton.disabled = false;
-        
+// Generate a random choice for the computer
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+// Play a single round and return the winner
+function playRound(playerChoice, computerChoice) {
+  let roundWinner = "";
+  switch (playerChoice + computerChoice) {
+    default:
+      roundWinner = "Tie game!";
+      break;
+    case "scissorspaper":
+    case "rockscissors":
+    case "paperrock":
+      roundWinner = "player";
+      break;
+    case "scissorsrock":
+    case "rockpaper":
+    case "paperscissors":
+      roundWinner = "computer";
+      break;
+  }
+  return roundWinner;
+}
+
+// Update the game state and the DOM based on the round winner
+function updateGame(roundWinner) {
+  switch (roundWinner) {
+    default:
+      console.log("Something went wrong...");
+      break;
+    case "Tie game!":
+      console.log("Draw");
+      break;
+    case "player":
+      playerScore++;
+      playerScoreEl.textContent = playerScore;
+      break;
+    case "computer":
+      computerScore++;
+      computerScoreEl.textContent = computerScore;
+      break;
+  }
+  roundCounter++;
+  roundsPlayedEl.textContent = roundCounter;
+
+  // Check for end of game
+  if (playerScore === 5) {
+    showGameOverMessage("You won!");
+  } else if (computerScore === 5) {
+    showGameOverMessage("You lost :(");
+  }
+}
+// Enable/disable the choice buttons
+function disableButtons(buttons) {
+    buttons.forEach((button) => {
+      button.disabled = true;
     });
-    playGame(playerChoiceButtons, computerScore, playerScore, roundCount);
+  }
+  
+  function enableButtons(buttons) {
+    buttons.forEach((button) => {
+      button.disabled = false;
+    });
+  }
+
+// Show the game over message and disable the choice buttons
+function showGameOverMessage(message) {
+  const gameOverEl = document.createElement("span");
+  gameOverEl.classList.add("game-over");
+  gameOverEl.textContent = message;
+  gameResultsSection.insertBefore(gameOverEl, newGameButton);
+  disableButtons(choiceButtons);
+  newGameButton.style.visibility = "visible";
+}
+
+
+
+// Start a new game
+function startNewGame() {
+  computerScore = 0;
+  playerScore = 0;
+  roundCounter = 0;
+  computerScoreEl.textContent = computerScore;
+  playerScoreEl.textContent = playerScore;
+  roundsPlayedEl.textContent = roundCounter;
+  gameResultsSection.removeChild(document.querySelector(".game-over"));
+  enableButtons(choiceButtons);
+  newGameButton.style.visibility = "hidden";
+}
+
+// Event listeners
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const roundWinner = playRound(button.value, getComputerChoice());
+    updateGame(roundWinner);
+  });
 });
 
+newGameButton.addEventListener("click", startNewGame);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// playerChoiceButtons.forEach(chosenButton => {
-//     chosenButton.addEventListener("click", () => {
-//         chosenButton.disabled = false;
-//         let roundResult = round(chosenButton.value, getComputerChoice(rockPaperScissors));
-    //     switch(roundResult){
-    //         default:
-    //                 console.log("Something wrong, I can feel it...");
-    //                 break;
-        
-    //         case "Tie game!":
-    //             console.log("Round draw!");
-    //             break;
-        
-    //         case "player":
-    //             playerWinsCount++;
-    //             playerScore.textContent = playerWinsCount;
-    //             console.log("PLAYER WON THIS ROUND!");
-    //             console.log(`playerScore = ${playerWinsCount}`);
-    //             break;
-        
-    //         case "computer":
-    //             computerWinsCount++;
-    //             computerScore.textContent = computerWinsCount;
-    //             console.log("COMPUTER WON THIS ROUND!");
-    //             console.log(`computerScore = ${computerWinsCount}`);
-    //             break;
-    //     }
-
-    // if(computerWinsCount === 5){
-    //     console.log("You lost this game :(");
-    //     playerWinsCount = 0;
-    //     computerWinsCount = 0;
-    //     playerScore.textContent = playerWinsCount;
-    //     computerScore.textContent = computerWinsCount;
-    //     playerChoiceButtons.forEach(chosenButton => {
-    //     chosenButton.disabled = true;
-    // });
-    //     return;
-    // }else if(playerWinsCount === 5){
-    //     console.log("You won this game!");
-    //     playerWinsCount = 0;
-    //     computerWinsCount = 0;
-    //     playerScore.textContent = playerWinsCount;
-    //     computerScore.textContent = computerWinsCount;
-    //     playerChoiceButtons.forEach(chosenButton => {
-    //         chosenButton.disabled = true;
-    //     });
-    //     return;
-    // }
-    // });
-// })
-
-
-
-
-// });
